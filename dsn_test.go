3cf7d4b3fd3d790e00a9c6da11ccc66f534687df
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-03-15 12:02:48 +0100

    test: Use assert.New(t) in all test cases

--- dsn_test.go
+++ dsn_test.go
@@ -8,120 +8,136 @@ import (
 )
 
 func TestDsnParsing(t *testing.T) {
+	assert := assert.New(t)
 	url := "https://username:password@domain:8888/foo/bar/23"
+
 	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
-	assert.Equal(t, SchemeHTTPS, dsn.scheme)
-	assert.Equal(t, "username", dsn.publicKey)
-	assert.Equal(t, "password", dsn.secretKey)
-	assert.Equal(t, "domain", dsn.host)
-	assert.Equal(t, 8888, dsn.port)
-	assert.Equal(t, "/foo/bar", dsn.path)
-	assert.Equal(t, 23, dsn.projectID)
-	assert.Equal(t, url, dsn.ToString())
+
+	assert.Nil(err)
+	assert.Equal(SchemeHTTPS, dsn.scheme)
+	assert.Equal("username", dsn.publicKey)
+	assert.Equal("password", dsn.secretKey)
+	assert.Equal("domain", dsn.host)
+	assert.Equal(8888, dsn.port)
+	assert.Equal("/foo/bar", dsn.path)
+	assert.Equal(23, dsn.projectID)
+	assert.Equal(url, dsn.ToString())
 }
 
 func TestDsnDefaultPort(t *testing.T) {
-	assert.Equal(t, 1337, Dsn{port: 1337}.Port())
-	assert.Equal(t, 1337, Dsn{scheme: "https", port: 1337}.Port())
-	assert.Equal(t, 443, Dsn{scheme: "https"}.Port())
-	assert.Equal(t, 80, Dsn{scheme: "http"}.Port())
+	assert := assert.New(t)
+
+	assert.Equal(1337, Dsn{port: 1337}.Port())
+	assert.Equal(1337, Dsn{scheme: "https", port: 1337}.Port())
+	assert.Equal(443, Dsn{scheme: "https"}.Port())
+	assert.Equal(80, Dsn{scheme: "http"}.Port())
 }
 
 func TestDsnSerializeDeserialize(t *testing.T) {
+	assert := assert.New(t)
 	url := "https://username:password@domain:8888/foo/bar/23"
-	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
+
+	dsn, dsnErr := NewDsn(url)
 	serialized, _ := json.Marshal(dsn)
-	assert.Equal(t, "\"https://username:password@domain:8888/foo/bar/23\"", string(serialized))
 	var deserialized Dsn
-	err = json.Unmarshal(serialized, &deserialized)
-	if err != nil {
-		assert.Fail(t, "Failed to unmarshal DSN JSON")
-	}
-	assert.Equal(t, url, deserialized.ToString())
+	unmarshalErr := json.Unmarshal(serialized, &deserialized)
+
+	assert.Nil(unmarshalErr)
+	assert.Nil(dsnErr)
+	assert.Equal("\"https://username:password@domain:8888/foo/bar/23\"", string(serialized))
+	assert.Equal(url, deserialized.ToString())
 }
 
 func TestDsnNoPort(t *testing.T) {
+	assert := assert.New(t)
 	url := "https://username@domain/42"
+
 	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
-	assert.Equal(t, 443, dsn.Port())
-	assert.Equal(t, url, dsn.ToString())
-	assert.Equal(t, "https://domain/api/42/store/", dsn.StoreAPIURL().String())
+
+	assert.Nil(err)
+	assert.Equal(443, dsn.Port())
+	assert.Equal(url, dsn.ToString())
+	assert.Equal("https://domain/api/42/store/", dsn.StoreAPIURL().String())
 }
 
 func TestInsecureDsnNoPort(t *testing.T) {
+	assert := assert.New(t)
 	url := "http://username@domain/42"
+
 	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
-	assert.Equal(t, 80, dsn.Port())
-	assert.Equal(t, url, dsn.ToString())
-	assert.Equal(t, "http://domain/api/42/store/", dsn.StoreAPIURL().String())
+
+	assert.Nil(err)
+	assert.Equal(80, dsn.Port())
+	assert.Equal(url, dsn.ToString())
+	assert.Equal("http://domain/api/42/store/", dsn.StoreAPIURL().String())
 }
 
 func TestDsnNoPassword(t *testing.T) {
+	assert := assert.New(t)
 	url := "https://username@domain:8888/42"
+
 	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
-	assert.Equal(t, url, dsn.ToString())
-	assert.Equal(t, "https://domain:8888/api/42/store/", dsn.StoreAPIURL().String())
+
+	assert.Nil(err)
+	assert.Equal(url, dsn.ToString())
+	assert.Equal("https://domain:8888/api/42/store/", dsn.StoreAPIURL().String())
 }
 
 func TestDsnHttpUrl(t *testing.T) {
+	assert := assert.New(t)
 	url := "http://username@domain:8888/42"
+
 	dsn, err := NewDsn(url)
-	if err != nil {
-		assert.Fail(t, "DsnParseError")
-	}
-	assert.Equal(t, url, dsn.ToString())
+
+	assert.Nil(err)
+	assert.Equal(url, dsn.ToString())
 }
 
 func TestDsnInvalidUrl(t *testing.T) {
+	assert := assert.New(t)
+
 	_, err := NewDsn("!@#$%^&*()")
-	if _, ok := err.(*DsnParseError); !ok {
-		assert.Fail(t, "should return DsnParseError")
-	}
-	assert.Equal(t, "DsnParseError: invalid url", err.Error())
+	_, ok := err.(*DsnParseError)
+
+	assert.True(ok)
+	assert.Equal("DsnParseError: invalid url", err.Error())
 }
 
 func TestDsnInvalidScheme(t *testing.T) {
+	assert := assert.New(t)
+
 	_, err := NewDsn("ftp://username:password@domain:8888/1")
-	if _, ok := err.(*DsnParseError); !ok {
-		assert.Fail(t, "should return DsnParseError")
-	}
-	assert.Equal(t, "DsnParseError: invalid scheme", err.Error())
+	_, ok := err.(*DsnParseError)
+
+	assert.True(ok)
+	assert.Equal("DsnParseError: invalid scheme", err.Error())
 }
 func TestDsnNoUsername(t *testing.T) {
+	assert := assert.New(t)
+
 	_, err := NewDsn("https://:password@domain:8888/23")
-	if _, ok := err.(*DsnParseError); !ok {
-		assert.Fail(t, "should return DsnParseError")
-	}
-	assert.Equal(t, "DsnParseError: empty username", err.Error())
+	_, ok := err.(*DsnParseError)
+
+	assert.True(ok)
+	assert.Equal("DsnParseError: empty username", err.Error())
 }
 
 func TestDsnNoHost(t *testing.T) {
+	assert := assert.New(t)
+
 	_, err := NewDsn("https://username:password@:8888/42")
-	if _, ok := err.(*DsnParseError); !ok {
-		assert.Fail(t, "should return DsnParseError")
-	}
-	assert.Equal(t, "DsnParseError: empty host", err.Error())
+	_, ok := err.(*DsnParseError)
+
+	assert.True(ok)
+	assert.Equal("DsnParseError: empty host", err.Error())
 }
 
 func TestDsnNoProjectId(t *testing.T) {
+	assert := assert.New(t)
+
 	_, err := NewDsn("https://username:password@domain:8888/")
-	if _, ok := err.(*DsnParseError); !ok {
-		assert.Fail(t, "should return DsnParseError")
-	}
-	assert.Equal(t, "DsnParseError: empty project id", err.Error())
+	_, ok := err.(*DsnParseError)
+
+	assert.True(ok)
+	assert.Equal("DsnParseError: empty project id", err.Error())
 }
--- scope_test.go
+++ scope_test.go
@@ -7,151 +7,174 @@ import (
 )
 
 func TestSetUser(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetUser(User{id: "foo"})
-	assert.Equal(t, User{id: "foo"}, scope.user)
+	assert.Equal(User{id: "foo"}, scope.user)
 }
 
 func TestSetUserOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetUser(User{id: "foo"})
 	scope.SetUser(User{id: "bar"})
-	assert.Equal(t, User{id: "bar"}, scope.user)
+	assert.Equal(User{id: "bar"}, scope.user)
 }
 
 func TestSetTag(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTag("a", "foo")
-	assert.Equal(t, map[string]string{"a": "foo"}, scope.tags)
+	assert.Equal(map[string]string{"a": "foo"}, scope.tags)
 }
 
 func TestSetTagMerges(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTag("a", "foo")
 	scope.SetTag("b", "bar")
-	assert.Equal(t, map[string]string{"a": "foo", "b": "bar"}, scope.tags)
+	assert.Equal(map[string]string{"a": "foo", "b": "bar"}, scope.tags)
 }
 
 func TestSetTagOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTag("a", "foo")
 	scope.SetTag("a", "bar")
-	assert.Equal(t, map[string]string{"a": "bar"}, scope.tags)
+	assert.Equal(map[string]string{"a": "bar"}, scope.tags)
 }
 
 func TestSetTags(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTags(map[string]string{"a": "foo"})
-	assert.Equal(t, map[string]string{"a": "foo"}, scope.tags)
+	assert.Equal(map[string]string{"a": "foo"}, scope.tags)
 }
 func TestSetTagsMerges(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTags(map[string]string{"a": "foo"})
 	scope.SetTags(map[string]string{"b": "bar", "c": "baz"})
-	assert.Equal(t, map[string]string{"a": "foo", "b": "bar", "c": "baz"}, scope.tags)
+	assert.Equal(map[string]string{"a": "foo", "b": "bar", "c": "baz"}, scope.tags)
 }
 
 func TestSetTagsOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetTags(map[string]string{"a": "foo"})
 	scope.SetTags(map[string]string{"a": "bar", "b": "baz"})
-	assert.Equal(t, map[string]string{"a": "bar", "b": "baz"}, scope.tags)
+	assert.Equal(map[string]string{"a": "bar", "b": "baz"}, scope.tags)
 }
 
 func TestSetExtra(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtra("a", 1)
-	assert.Equal(t, map[string]interface{}{"a": 1}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": 1}, scope.extra)
 }
 func TestSetExtraMerges(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtra("a", "foo")
 	scope.SetExtra("b", 2)
-	assert.Equal(t, map[string]interface{}{"a": "foo", "b": 2}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": "foo", "b": 2}, scope.extra)
 }
 
 func TestSetExtraOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtra("a", "foo")
 	scope.SetExtra("a", 2)
-	assert.Equal(t, map[string]interface{}{"a": 2}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": 2}, scope.extra)
 }
 
 func TestSetExtras(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtras(map[string]interface{}{"a": 1})
-	assert.Equal(t, map[string]interface{}{"a": 1}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": 1}, scope.extra)
 }
 func TestSetExtrasMerges(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtras(map[string]interface{}{"a": "foo"})
 	scope.SetExtras(map[string]interface{}{"b": 2, "c": 3})
-	assert.Equal(t, map[string]interface{}{"a": "foo", "b": 2, "c": 3}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": "foo", "b": 2, "c": 3}, scope.extra)
 }
 
 func TestSetExtrasOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetExtras(map[string]interface{}{"a": "foo"})
 	scope.SetExtras(map[string]interface{}{"a": 2, "b": 3})
-	assert.Equal(t, map[string]interface{}{"a": 2, "b": 3}, scope.extra)
+	assert.Equal(map[string]interface{}{"a": 2, "b": 3}, scope.extra)
 }
 func TestSetFingerprint(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetFingerprint([]string{"abcd"})
-	assert.Equal(t, []string{"abcd"}, scope.fingerprint)
+	assert.Equal([]string{"abcd"}, scope.fingerprint)
 }
 
 func TestSetFingerprintOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetFingerprint([]string{"abc"})
 	scope.SetFingerprint([]string{"def"})
-	assert.Equal(t, []string{"def"}, scope.fingerprint)
+	assert.Equal([]string{"def"}, scope.fingerprint)
 }
 
 func TestSetLevel(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetLevel(LevelInfo)
-	assert.Equal(t, LevelInfo, scope.level)
+	assert.Equal(LevelInfo, scope.level)
 }
 
 func TestSetLevelOverrides(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.SetLevel(LevelInfo)
 	scope.SetLevel(LevelFatal)
-	assert.Equal(t, LevelFatal, scope.level)
+	assert.Equal(LevelFatal, scope.level)
 }
 
 func TestAddBreadcrumb(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.AddBreadcrumb(Breadcrumb{message: "test"})
-	assert.Equal(t, []Breadcrumb{{message: "test"}}, scope.breadcrumbs)
+	assert.Equal([]Breadcrumb{{message: "test"}}, scope.breadcrumbs)
 }
 
 func TestAddBreadcrumbAppends(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.AddBreadcrumb(Breadcrumb{message: "test1"})
 	scope.AddBreadcrumb(Breadcrumb{message: "test2"})
 	scope.AddBreadcrumb(Breadcrumb{message: "test3"})
-	assert.Equal(t, []Breadcrumb{{message: "test1"}, {message: "test2"}, {message: "test3"}}, scope.breadcrumbs)
+	assert.Equal([]Breadcrumb{{message: "test1"}, {message: "test2"}, {message: "test3"}}, scope.breadcrumbs)
 }
 
 func TestAddBreadcrumbDefaultLimit(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	for i := 0; i < 101; i++ {
 		scope.AddBreadcrumb(Breadcrumb{message: "test"})
 	}
-	assert.Equal(t, 100, len(scope.breadcrumbs))
+	assert.Len(scope.breadcrumbs, 100)
 }
 
 func TestBasicInheritance(t *testing.T) {
+	assert := assert.New(t)
 	parentScope := NewScope()
 	parentScope.SetExtra("a", 1)
 	scope := parentScope.Clone()
-	assert.Equal(t, parentScope.extra, scope.extra)
+	assert.Equal(parentScope.extra, scope.extra)
 }
 
 // TODO: TEST OTHER
 func TestParentChangedInheritance(t *testing.T) {
+	assert := assert.New(t)
 	parentScope := NewScope()
 	scope := parentScope.Clone()
 
@@ -169,22 +192,23 @@ func TestParentChangedInheritance(t *testing.T) {
 	parentScope.AddBreadcrumb(Breadcrumb{message: "bar"})
 	parentScope.SetUser(User{id: "bar"})
 
-	assert.Equal(t, map[string]string{"foo": "bar"}, scope.tags)
-	assert.Equal(t, map[string]interface{}{"foo": "bar"}, scope.extra)
-	assert.Equal(t, LevelDebug, scope.level)
-	assert.Equal(t, []string{"foo"}, scope.fingerprint)
-	assert.Equal(t, []Breadcrumb{{message: "foo"}}, scope.breadcrumbs)
-	assert.Equal(t, User{id: "foo"}, scope.user)
+	assert.Equal(map[string]string{"foo": "bar"}, scope.tags)
+	assert.Equal(map[string]interface{}{"foo": "bar"}, scope.extra)
+	assert.Equal(LevelDebug, scope.level)
+	assert.Equal([]string{"foo"}, scope.fingerprint)
+	assert.Equal([]Breadcrumb{{message: "foo"}}, scope.breadcrumbs)
+	assert.Equal(User{id: "foo"}, scope.user)
 
-	assert.Equal(t, map[string]string{"foo": "baz"}, parentScope.tags)
-	assert.Equal(t, map[string]interface{}{"foo": "baz"}, parentScope.extra)
-	assert.Equal(t, LevelFatal, parentScope.level)
-	assert.Equal(t, []string{"bar"}, parentScope.fingerprint)
-	assert.Equal(t, []Breadcrumb{{message: "bar"}}, parentScope.breadcrumbs)
-	assert.Equal(t, User{id: "bar"}, parentScope.user)
+	assert.Equal(map[string]string{"foo": "baz"}, parentScope.tags)
+	assert.Equal(map[string]interface{}{"foo": "baz"}, parentScope.extra)
+	assert.Equal(LevelFatal, parentScope.level)
+	assert.Equal([]string{"bar"}, parentScope.fingerprint)
+	assert.Equal([]Breadcrumb{{message: "bar"}}, parentScope.breadcrumbs)
+	assert.Equal(User{id: "bar"}, parentScope.user)
 }
 
 func TestChildOverrideInheritance(t *testing.T) {
+	assert := assert.New(t)
 	parentScope := NewScope()
 	parentScope.SetTag("foo", "baz")
 	parentScope.SetExtra("foo", "baz")
@@ -201,22 +225,23 @@ func TestChildOverrideInheritance(t *testing.T) {
 	scope.AddBreadcrumb(Breadcrumb{message: "foo"})
 	scope.SetUser(User{id: "foo"})
 
-	assert.Equal(t, map[string]string{"foo": "bar"}, scope.tags)
-	assert.Equal(t, map[string]interface{}{"foo": "bar"}, scope.extra)
-	assert.Equal(t, LevelDebug, scope.level)
-	assert.Equal(t, []string{"foo"}, scope.fingerprint)
-	assert.Equal(t, []Breadcrumb{{message: "foo"}}, scope.breadcrumbs)
-	assert.Equal(t, User{id: "foo"}, scope.user)
+	assert.Equal(map[string]string{"foo": "bar"}, scope.tags)
+	assert.Equal(map[string]interface{}{"foo": "bar"}, scope.extra)
+	assert.Equal(LevelDebug, scope.level)
+	assert.Equal([]string{"foo"}, scope.fingerprint)
+	assert.Equal([]Breadcrumb{{message: "foo"}}, scope.breadcrumbs)
+	assert.Equal(User{id: "foo"}, scope.user)
 
-	assert.Equal(t, map[string]string{"foo": "baz"}, parentScope.tags)
-	assert.Equal(t, map[string]interface{}{"foo": "baz"}, parentScope.extra)
-	assert.Equal(t, LevelFatal, parentScope.level)
-	assert.Equal(t, []string{"bar"}, parentScope.fingerprint)
-	assert.Equal(t, []Breadcrumb{{message: "bar"}}, parentScope.breadcrumbs)
-	assert.Equal(t, User{id: "bar"}, parentScope.user)
+	assert.Equal(map[string]string{"foo": "baz"}, parentScope.tags)
+	assert.Equal(map[string]interface{}{"foo": "baz"}, parentScope.extra)
+	assert.Equal(LevelFatal, parentScope.level)
+	assert.Equal([]string{"bar"}, parentScope.fingerprint)
+	assert.Equal([]Breadcrumb{{message: "bar"}}, parentScope.breadcrumbs)
+	assert.Equal(User{id: "bar"}, parentScope.user)
 }
 
 func TestClear(t *testing.T) {
+	assert := assert.New(t)
 	scope := NewScope()
 	scope.AddBreadcrumb(Breadcrumb{message: "test"})
 	scope.SetUser(User{id: "1"})
@@ -225,10 +250,10 @@ func TestClear(t *testing.T) {
 	scope.SetFingerprint([]string{"abcd"})
 	scope.SetLevel(LevelFatal)
 	scope.Clear()
-	assert.Equal(t, []Breadcrumb{}, scope.breadcrumbs)
-	assert.Equal(t, User{}, scope.user)
-	assert.Equal(t, map[string]string{}, scope.tags)
-	assert.Equal(t, map[string]interface{}{}, scope.extra)
-	assert.Equal(t, []string{}, scope.fingerprint)
-	assert.Equal(t, LevelInfo, scope.level)
+	assert.Equal([]Breadcrumb{}, scope.breadcrumbs)
+	assert.Equal(User{}, scope.user)
+	assert.Equal(map[string]string{}, scope.tags)
+	assert.Equal(map[string]interface{}{}, scope.extra)
+	assert.Equal([]string{}, scope.fingerprint)
+	assert.Equal(LevelInfo, scope.level)
 }
