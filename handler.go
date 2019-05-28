ae7f25b4881fb1b0fa585420cdc3929ffca65b41
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-05-28 16:25:44 +0200

    ref: Extract context creation in handlers to helper method

--- handler.go
+++ handler.go
@@ -8,21 +8,9 @@ import (
 // Decorate wraps `http.Handler` and recovers from all the panics, providing necessary `Hub`
 // instance that is bound to the request `Context` object.
 func Decorate(handler http.Handler) http.Handler {
-	currentHub := CurrentHub()
-
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		client := currentHub.Client()
-		scope := currentHub.Scope().Clone()
-		scope.SetRequest(Request{}.FromHTTPRequest(request))
-		hub := NewHub(client, scope)
-
-		ctx := request.Context()
-		ctx = context.WithValue(ctx, ResponseContextKey, response)
-		ctx = context.WithValue(ctx, RequestContextKey, request)
-		ctx = SetHubOnContext(ctx, hub)
-
+		ctx := createContextWithHub(response, request)
 		defer RecoverWithContext(ctx)
-
 		handler.ServeHTTP(response, request.WithContext(ctx))
 	})
 }
@@ -30,20 +18,22 @@ func Decorate(handler http.Handler) http.Handler {
 // DecorateFunc wraps `http.HandlerFunc` and recovers from all the panics, providing necessary `Hub`
 // instance that is bound to the request `Context` object.
 func DecorateFunc(handler http.HandlerFunc) http.HandlerFunc {
-	currentHub := CurrentHub()
 
 	return func(response http.ResponseWriter, request *http.Request) {
-		client := currentHub.Client()
-		scope := currentHub.Scope().Clone()
-		scope.SetRequest(Request{}.FromHTTPRequest(request))
-		hub := NewHub(client, scope)
-
-		ctx := request.Context()
-		ctx = context.WithValue(ctx, ResponseContextKey, response)
-		ctx = context.WithValue(ctx, RequestContextKey, request)
-		ctx = SetHubOnContext(ctx, hub)
+		ctx := createContextWithHub(response, request)
 		defer RecoverWithContext(ctx)
-
 		handler(response, request.WithContext(ctx))
 	}
+}
+
+func createContextWithHub(response http.ResponseWriter, request *http.Request) context.Context {
+	client := CurrentHub().Client()
+	scope := CurrentHub().Scope().Clone()
+	scope.SetRequest(Request{}.FromHTTPRequest(request))
+
+	ctx := request.Context()
+	ctx = context.WithValue(ctx, ResponseContextKey, response)
+	ctx = context.WithValue(ctx, RequestContextKey, request)
+
+	return SetHubOnContext(ctx, NewHub(client, scope))
 }
