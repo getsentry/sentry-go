5ead4120c22a73daefe9d745f3ead883fdc44884
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-07-26 10:44:06 +0200

    release: 0.2.0

--- sentry.go
+++ sentry.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version Sentry-Go SDK Version
-const Version = "0.1.3"
+const Version = "0.2.0"
 
 // Init initializes whole SDK by creating new `Client` and binding it to the current `Hub`
 func Init(options ClientOptions) error {
