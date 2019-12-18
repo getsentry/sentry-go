75cd07691eaa58def3d67eaf9a948385650534c8
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2019-12-18 19:23:47 +0100

    fix(stacktrace): Correctly report package names
    Makes function receivers part of the function name and not part of the
    package name.

--- stacktrace_test.go
+++ stacktrace_test.go
@@ -1,8 +1,9 @@
 package sentry
 
 import (
-	"runtime"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func NewStacktraceForTest() *Stacktrace {
@@ -15,28 +16,52 @@ func (StacktraceTestHelper) NewStacktrace() *Stacktrace {
 	return NewStacktrace()
 }
 
-func TestFunctionName(t *testing.T) {
-	for _, test := range []struct {
-		skip int
-		pack string
-		name string
-	}{
-		{0, "sentry-go", "TestFunctionName"},
-		{1, "testing", "tRunner"},
-		{2, "runtime", "goexit"},
-		{100, "", ""},
-	} {
-		pc, _, _, _ := runtime.Caller(test.skip)
-		pack, name := deconstructFunctionName(runtime.FuncForPC(pc).Name())
-
-		// Go1.10 reports different paths than Modules aware versions (>=1.11)
-		assertStringContains(t, pack, test.pack)
-		assertEqual(t, name, test.name)
-	}
-}
-
 func BenchmarkNewStacktrace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Trace()
+	}
+}
+
+//nolint: scopelint // false positive https://github.com/kyoh86/scopelint/issues/4
+func TestSplitQualifiedFunctionName(t *testing.T) {
+	tests := []struct {
+		in  string
+		pkg string
+		fun string
+	}{
+		{"", "", ""},
+		{"runtime.Callers", "runtime", "Callers"},
+		{"main.main.func1", "main", "main.func1"},
+		{
+			"github.com/getsentry/sentry-go.Init",
+			"github.com/getsentry/sentry-go",
+			"Init",
+		},
+		{
+			"github.com/getsentry/sentry-go.(*Hub).Flush",
+			"github.com/getsentry/sentry-go",
+			"(*Hub).Flush",
+		},
+		{
+			"github.com/getsentry/sentry-go.Test.func2.1.1",
+			"github.com/getsentry/sentry-go",
+			"Test.func2.1.1",
+		},
+		{
+			"github.com/getsentry/confusing%2epkg%2ewith%2edots.Test.func1",
+			"github.com/getsentry/confusing%2epkg%2ewith%2edots",
+			"Test.func1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			pkg, fun := splitQualifiedFunctionName(tt.in)
+			if diff := cmp.Diff(tt.pkg, pkg); diff != "" {
+				t.Errorf("Package name mismatch (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(tt.fun, fun); diff != "" {
+				t.Errorf("Function name mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
