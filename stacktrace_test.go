getsentry/sentry-go

commit 61d7ccc9c52727d6ea221ff9327873c78ea41d1c

fix(stacktrace): Do not rely on AbsPath of files
NewStacktrace and ExtractStacktrace (through filterFrames) used to
whitelist stack frames of test and example files that are part of
sentry-go. That is likely only done to allow for testing.

However, by relying on the absolute path of files to perform the
filtering, the correct filtering of frames depended on how sentry-go was
checked out on disk. If the repository was checked out as part of a
traditional GOPATH tree, tests passed. If the repository was outside of
GOPATH, on an arbitraly named path (without a github.com/getsentry/
prefix), tests failed.

This commit replaces tests from stacktrace_test.go with new tests in
stacktrace_external_test.go. The difference being that the new tests are
built as a separate package. The separation is achieved by using the
ability to suffix the test package name with '_test'. See 'go help
test'.

This new test structure makes it easier to distinguish stack frames that
belong to the sentry-go SDK vs. stack frames that are part of the tests,
thus removing the need for path-pattern-based filtering.

The new tests make a more in-depth comparison of the stack frames
against expected values.

Now NewStacktrace and ExtractStacktrace filter out all the frames that
are part of the github.com/getsentry/sentry-go package/Module, without
looking at AbsPath.

stacktrace.go
@@ -4,18 +4,11 @@ import (
 	"go/build"
 	"path/filepath"
 	"reflect"
-	"regexp"
 	"runtime"
 	"strings"
 )
 
 const unknown string = "unknown"
-
-//nolint: gochecknoglobals
-var (
-	isTestFileRegexp    = regexp.MustCompile(`getsentry/sentry-go/.+_test.go`)
-	isExampleFileRegexp = regexp.MustCompile(`getsentry/sentry-go/example/`)
-)
 
 // The module download is split into two parts: downloading the go.mod and downloading the actual code.
 // If you have dependencies only needed for tests, then they will show up in your go.mod,
@@ -213,11 +206,7 @@ func filterFrames(frames []Frame) []Frame {
 			continue
 		}
 		// sentry internal frames
-		isTestFile := isTestFileRegexp.MatchString(frame.AbsPath)
-		isExampleFile := isExampleFileRegexp.MatchString(frame.AbsPath)
-		if strings.Contains(frame.AbsPath, "github.com/getsentry/sentry-go") &&
-			!isTestFile &&
-			!isExampleFile {
+		if frame.Module == "github.com/getsentry/sentry-go" {
 			continue
 		}
 		filteredFrames = append(filteredFrames, frame)

stacktrace_test.go
@@ -1,10 +1,13 @@
 package sentry
 
 import (
-	"path/filepath"
 	"runtime"
 	"testing"
 )
+
+func NewStacktraceForTest() *Stacktrace {
+	return NewStacktrace()
+}
 
 func TestFunctionName(t *testing.T) {
 	for _, test := range []struct {
@@ -26,67 +29,8 @@ func TestFunctionName(t *testing.T) {
 	}
 }
 
-func TestNewStacktrace(t *testing.T) {
-	stacktrace := Trace()
-
-	assertEqual(t, len(stacktrace.Frames), 2)
-	assertEqual(t, stacktrace.Frames[0].Function, "TestNewStacktrace")
-	assertEqual(t, stacktrace.Frames[0].Filename, "stacktrace_test.go")
-	assertEqual(t, stacktrace.Frames[1].Function, "Trace")
-	assertEqual(t, stacktrace.Frames[1].Filename, "errors_test.go")
-}
-
 func BenchmarkNewStacktrace(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Trace()
 	}
-}
-
-func TestStacktraceFrame(t *testing.T) {
-	_, callerFile, _, _ := runtime.Caller(0)
-	dir, _ := filepath.Split(callerFile)
-
-	stacktrace := Trace()
-	frame := stacktrace.Frames[len(stacktrace.Frames)-1]
-
-	assertEqual(t, frame.Filename, "errors_test.go")
-	assertEqual(t, frame.AbsPath, filepath.Join(dir, "errors_test.go"))
-	assertEqual(t, frame.Function, "Trace")
-	assertEqual(t, frame.Lineno, 12)
-	assertEqual(t, frame.InApp, true)
-	// Go1.10 reports different Module than Modules aware versions (>=1.11)
-	assertStringContains(t, frame.Module, "sentry-go")
-}
-
-// https://github.com/pkg/errors
-func TestExtractStacktracePkgErrors(t *testing.T) {
-	err := RedPkgErrorsRanger()
-	stacktrace := ExtractStacktrace(err)
-
-	assertEqual(t, len(stacktrace.Frames), 3)
-	assertEqual(t, stacktrace.Frames[0].Function, "TestExtractStacktracePkgErrors")
-	assertEqual(t, stacktrace.Frames[1].Function, "RedPkgErrorsRanger")
-	assertEqual(t, stacktrace.Frames[2].Function, "BluePkgErrorsRanger")
-}
-
-// https://github.com/pingcap/errors
-func TestExtractStacktracePingcapErrors(t *testing.T) {
-	err := RedPingcapErrorsRanger()
-	stacktrace := ExtractStacktrace(err)
-
-	assertEqual(t, len(stacktrace.Frames), 3)
-	assertEqual(t, stacktrace.Frames[0].Function, "TestExtractStacktracePingcapErrors")
-	assertEqual(t, stacktrace.Frames[1].Function, "RedPingcapErrorsRanger")
-	assertEqual(t, stacktrace.Frames[2].Function, "BluePingcapErrorsRanger")
-}
-
-// https://github.com/go-errors/errors
-func TestExtractStacktraceGoErrors(t *testing.T) {
-	err := RedGoErrorsRanger()
-	stacktrace := ExtractStacktrace(err)
-
-	assertEqual(t, len(stacktrace.Frames), 3)
-	assertEqual(t, stacktrace.Frames[0].Function, "TestExtractStacktraceGoErrors")
-	assertEqual(t, stacktrace.Frames[1].Function, "RedGoErrorsRanger")
-	assertEqual(t, stacktrace.Frames[2].Function, "BlueGoErrorsRanger")
 }

.golangci.yml
@@ -3,6 +3,7 @@ linters:
   disable:
     - megacheck
     - stylecheck
+    - funlen
 run:
   skip-dirs:
     - echo

stacktrace_external_test.go
@@ -1,0 +1,228 @@
+package sentry_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	goErrors "github.com/go-errors/errors"
+	pingcapErrors "github.com/pingcap/errors"
+	pkgErrors "github.com/pkg/errors"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func f1() *sentry.Stacktrace {
+	return sentry.NewStacktrace()
+}
+
+func f2() *sentry.Stacktrace {
+	return f1()
+}
+func f3() *sentry.Stacktrace {
+	return sentry.NewStacktraceForTest()
+}
+
+func RedPkgErrorsRanger() error {
+	return BluePkgErrorsRanger()
+}
+
+func BluePkgErrorsRanger() error {
+	return pkgErrors.New("this is bad from pkgErrors")
+}
+
+func RedPingcapErrorsRanger() error {
+	return BluePingcapErrorsRanger()
+}
+
+func BluePingcapErrorsRanger() error {
+	return pingcapErrors.New("this is bad from pingcapErrors")
+}
+
+func RedGoErrorsRanger() error {
+	return BlueGoErrorsRanger()
+}
+
+func BlueGoErrorsRanger() error {
+	return goErrors.New("this is bad from goErrors")
+}
+
+//nolint: scopelint // false positive https://github.com/kyoh86/scopelint/issues/4
+func TestNewStacktrace(t *testing.T) {
+	tests := map[string]struct {
+		f    func() *sentry.Stacktrace
+		want *sentry.Stacktrace
+	}{
+		"f1": {f1, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "f1",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   18,
+					InApp:    true,
+				},
+			},
+		}},
+		"f2": {f2, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "f2",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   22,
+					InApp:    true,
+				},
+				{
+					Function: "f1",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   18,
+					InApp:    true,
+				},
+			},
+		}},
+		// test that functions in the SDK that call NewStacktrace are not part
+		// of the resulting Stacktrace.
+		"NewStacktraceForTest": {sentry.NewStacktraceForTest, &sentry.Stacktrace{
+			Frames: []sentry.Frame{},
+		}},
+		"f3": {f3, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "f3",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   25,
+					InApp:    true,
+				},
+			},
+		}},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tt.f()
+			compareStacktrace(t, got, tt.want)
+		})
+	}
+}
+
+//nolint: scopelint // false positive https://github.com/kyoh86/scopelint/issues/4
+func TestExtractStacktrace(t *testing.T) {
+	tests := map[string]struct {
+		f    func() error
+		want *sentry.Stacktrace
+	}{
+		// https://github.com/pkg/errors
+		"pkg/errors": {RedPkgErrorsRanger, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "RedPkgErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   29,
+					InApp:    true,
+				},
+				{
+					Function: "BluePkgErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   33,
+					InApp:    true,
+				},
+			},
+		}},
+		// https://github.com/pingcap/errors
+		"pingcap/errors": {RedPingcapErrorsRanger, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "RedPingcapErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   37,
+					InApp:    true,
+				},
+				{
+					Function: "BluePingcapErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   41,
+					InApp:    true,
+				},
+			},
+		}},
+		// https://github.com/go-errors/errors
+		"go-errors/errors": {RedGoErrorsRanger, &sentry.Stacktrace{
+			Frames: []sentry.Frame{
+				{
+					Function: "RedGoErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   45,
+					InApp:    true,
+				},
+				{
+					Function: "BlueGoErrorsRanger",
+					Module:   "github.com/getsentry/sentry-go_test",
+					Filename: "stacktrace_external_test.go",
+					AbsPath:  "ignored",
+					Lineno:   49,
+					InApp:    true,
+				},
+			},
+		}},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := tt.f()
+			if err == nil {
+				t.Fatal("got nil error")
+			}
+			got := sentry.ExtractStacktrace(err)
+			compareStacktrace(t, got, tt.want)
+		})
+	}
+}
+
+func compareStacktrace(t *testing.T, got, want *sentry.Stacktrace) {
+	t.Helper()
+
+	if len(got.Frames) == 0 {
+		t.Fatal("got no frames")
+	}
+	// Skip anonymous function passed to t.Run.
+	got.Frames = got.Frames[1:]
+
+	if diff := stacktraceDiff(want, got); diff != "" {
+		t.Fatalf("Stacktrace mismatch (-want +got):\n%s", diff)
+	}
+
+	// Because stacktraceDiff ignores Frame.AbsPath, sanity check that the
+	// values we got are actually absolute paths pointing to this test file.
+	for _, frame := range got.Frames {
+		if !filepath.IsAbs(frame.AbsPath) {
+			t.Errorf("Frame{Function: %q}.AbsPath = %q, want absolute path", frame.Function, frame.AbsPath)
+		}
+		if filepath.Base(frame.AbsPath) != "stacktrace_external_test.go" {
+			t.Errorf(`Frame{Function: %q}.AbsPath = %q, want ".../stacktrace_external_test.go"`, frame.Function, frame.AbsPath)
+		}
+	}
+}
+
+func stacktraceDiff(x, y *sentry.Stacktrace) string {
+	return cmp.Diff(
+		x, y,
+		cmpopts.IgnoreFields(sentry.Frame{}, "AbsPath"),
+	)
+}

commit bf38760c42148790549ebc2a748bbdbfe7bb444a

fix(stacktrace): Ignore SDK subpackage frames
Compared to release v0.3.1, now filteredFrames compares the frame.Module
value instead of frame.AbsPath, and ignores SDK "_test" packages instead
of pattern matching test files and examples.

However confusing it might be, frame.Module holds a package name,
unrelated to Go Modules.

This fixes up the regression introduced in
61d7ccc9c52727d6ea221ff9327873c78ea41d1c, which caused frames from
integrations/subpackages to be reported to Sentry.

stacktrace.go
@@ -206,17 +206,24 @@ func extractFrames(pcs []uintptr) []Frame {
 	return frames
 }
 
+// filterFrames filters out stack frames that are not meant to be reported to
+// Sentry. Those are frames internal to the SDK or Go.
 func filterFrames(frames []Frame) []Frame {
+	if len(frames) == 0 {
+		return nil
+	}
+
 	filteredFrames := make([]Frame, 0, len(frames))
 
 	for _, frame := range frames {
-		// go runtime frames
+		// Skip Go internal frames.
 		if frame.Module == "runtime" || frame.Module == "testing" {
 			continue
 		}
-		// sentry internal frames
-		if frame.Module == "github.com/getsentry/sentry-go" ||
-			strings.HasPrefix(frame.Module, "github.com/getsentry/sentry-go.") {
+		// Skip Sentry internal frames, except for frames in _test packages (for
+		// testing).
+		if strings.HasPrefix(frame.Module, "github.com/getsentry/sentry-go") &&
+			!strings.HasSuffix(frame.Module, "_test") {
 			continue
 		}
 		filteredFrames = append(filteredFrames, frame)

stacktrace_test.go
@@ -65,3 +65,97 @@ func TestSplitQualifiedFunctionName(t *testing.T) {
 		})
 	}
 }
+
+//nolint: scopelint // false positive https://github.com/kyoh86/scopelint/issues/4
+func TestFilterFrames(t *testing.T) {
+	tests := []struct {
+		in  []Frame
+		out []Frame
+	}{
+		// sanity check
+		{},
+		// filter out go internals and SDK internals; "sentry-go_test" is
+		// considered outside of the SDK and thus included (useful for testing)
+		{
+			in: []Frame{
+				{
+					Function: "goexit",
+					Module:   "runtime",
+					AbsPath:  "/goroot/src/runtime/asm_amd64.s",
+					InApp:    false,
+				},
+				{
+					Function: "tRunner",
+					Module:   "testing",
+					AbsPath:  "/goroot/src/testing/testing.go",
+					InApp:    false,
+				},
+				{
+					Function: "TestNewStacktrace.func1",
+					Module:   "github.com/getsentry/sentry-go_test",
+					AbsPath:  "/somewhere/sentry/sentry-go/stacktrace_external_test.go",
+					InApp:    true,
+				},
+				{
+					Function: "StacktraceTestHelper.NewStacktrace",
+					Module:   "github.com/getsentry/sentry-go",
+					AbsPath:  "/somewhere/sentry/sentry-go/stacktrace_test.go",
+					InApp:    true,
+				},
+				{
+					Function: "NewStacktrace",
+					Module:   "github.com/getsentry/sentry-go",
+					AbsPath:  "/somewhere/sentry/sentry-go/stacktrace.go",
+					InApp:    true,
+				},
+			},
+			out: []Frame{
+				{
+					Function: "TestNewStacktrace.func1",
+					Module:   "github.com/getsentry/sentry-go_test",
+					AbsPath:  "/somewhere/sentry/sentry-go/stacktrace_external_test.go",
+					InApp:    true,
+				},
+			},
+		},
+		// filter out integrations; SDK subpackages
+		{
+			in: []Frame{
+				{
+					Function: "Example.Integration",
+					Module:   "github.com/getsentry/sentry-go/http/integration",
+					AbsPath:  "/somewhere/sentry/sentry-go/http/integration/integration.go",
+					InApp:    true,
+				},
+				{
+					Function: "(*Handler).Handle",
+					Module:   "github.com/getsentry/sentry-go/http",
+					AbsPath:  "/somewhere/sentry/sentry-go/http/sentryhttp.go",
+					InApp:    true,
+				},
+				{
+					Function: "main",
+					Module:   "main",
+					AbsPath:  "/somewhere/example.com/pkg/main.go",
+					InApp:    true,
+				},
+			},
+			out: []Frame{
+				{
+					Function: "main",
+					Module:   "main",
+					AbsPath:  "/somewhere/example.com/pkg/main.go",
+					InApp:    true,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := filterFrames(tt.in)
+			if diff := cmp.Diff(tt.out, got); diff != "" {
+				t.Errorf("filterFrames() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}