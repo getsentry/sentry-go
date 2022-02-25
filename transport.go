c40f30a89cdda4cb59239f2f77466d3d4219de61
Kamil Ogórek <kamil@sentry.io>
2022-02-25 15:07:21 +0100

    misc: Update deprecated linters and fix code for remaining rules (#418)
    * misc: Update deprecated linters and fix code for remaining rules

--- transport.go
+++ transport.go
@@ -53,6 +53,8 @@ func getProxyConfig(options ClientOptions) func(*http.Request) (*url.URL, error)
 
 func getTLSConfig(options ClientOptions) *tls.Config {
 	if options.CaCerts != nil {
+		//#nosec G402 -- We should be using `MinVersion: tls.VersionTLS12`,
+		// 				 but we don't want to break peoples code without the major bump.
 		return &tls.Config{
 			RootCAs: options.CaCerts,
 		}
