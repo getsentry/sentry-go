a216e41385fe70c5117c4ac9c5a6990239b416df
GTB3NW <GTB3NW@users.noreply.github.com>
2022-06-22 18:36:23 +0100

    Add StripSpanContextKeyFromContext function
    StripSpanContextKeyFromContext returns a new context with the parent span removed
    but critically retaining the parentSpanID, allowing for new transactions
    to be formed within the same running process while retaining all the current
    context benefits, including existing sentry context keys (apart  from
    `spanContextKey`) and timeouts etc.

--- tracing.go
+++ tracing.go
@@ -626,3 +626,12 @@ func spanFromContext(ctx context.Context) *Span {
 	}
 	return nil
 }
+
+// StripSpanContextKeyFromContext returns a new context with the parent span removed
+// but critically retaining the parentSpanID, allowing for new transactions
+// to be formed within the same running process while retaining all the current
+// context benefits, including existing sentry context keys (apart  from
+// `spanContextKey`) and timeouts etc.
+func StripSpanContextKeyFromContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, spanContextKey{}, nil)
+}
