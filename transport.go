fba5ea44f0678136c2f8d0b11daa63ff2eea1bdf
Michi Hoffmann <cleptric@users.noreply.github.com>
2022-12-05 11:11:37 +0100

    Update transport.go
    Co-authored-by: Abhijeet Prasad <devabhiprasad@gmail.com>

--- transport.go
+++ transport.go
@@ -98,7 +98,7 @@ func transactionEnvelopeFromBody(event *Event, dsn *Dsn, sentAt time.Time, body 
 	var b bytes.Buffer
 	enc := json.NewEncoder(&b)
 
-	// Construct the trace envolope header
+	// Construct the trace envelope header
 	var trace = map[string]string{}
 	if dsc := event.sdkMetaData.dsc; dsc.HasEntries() {
 		for k, v := range dsc.Entries {
