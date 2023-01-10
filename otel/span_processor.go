2aa76fb95a341b0bc35003923c0a019b63187d1b
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-01-10 14:43:23 +0100

    OnStart

--- otel/span_processor.go
+++ otel/span_processor.go
@@ -5,35 +5,49 @@ import (
 	"time"
 
 	"github.com/getsentry/sentry-go"
-	"go.opentelemetry.io/otel/sdk/trace"
+	sdkTrace "go.opentelemetry.io/otel/sdk/trace"
+	trace "go.opentelemetry.io/otel/trace"
 )
 
-type sentrySpanProcessor struct{}
+type sentrySpanProcessor struct {
+	SpanMap map[trace.SpanID]*sentry.Span
+}
 
-var _ trace.SpanProcessor = (*sentrySpanProcessor)(nil)
+var _ sdkTrace.SpanProcessor = (*sentrySpanProcessor)(nil)
 
-func NewSentrySpanProcessor() trace.SpanProcessor {
-	ssp := &sentrySpanProcessor{}
+func NewSentrySpanProcessor() sdkTrace.SpanProcessor {
+	ssp := &sentrySpanProcessor{
+		SpanMap: map[trace.SpanID]*sentry.Span{},
+	}
 
 	return ssp
 }
 
-func (ssp *sentrySpanProcessor) OnStart(parent context.Context, s trace.ReadWriteSpan) {
-	hub := sentry.GetHubFromContext(parent)
-	if hub == nil {
-		return
+func (ssp *sentrySpanProcessor) OnStart(parent context.Context, s sdkTrace.ReadWriteSpan) {
+	otelSpanId := s.SpanContext().SpanID()
+	otelParentSpanId := s.Parent().SpanID()
+
+	var sentryParentSpan *sentry.Span
+	if otelParentSpanId.IsValid() {
+		sentryParentSpan = ssp.SpanMap[otelParentSpanId]
 	}
 
-	scope := hub.Scope()
-	if scope == nil {
-		return
-	}
+	if sentryParentSpan != nil {
+		span := sentryParentSpan.StartChild(s.Name())
+		span.SpanID = sentry.SpanID(otelSpanId)
+		span.StartTime = s.StartTime()
 
-	otelSpanId := s.SpanContext().SpanID().String()
-	otelParentSpanId = s.Parent().SpanID().String()
+		ssp.SpanMap[otelSpanId] = span
+	} else {
+		transaction := sentry.StartTransaction(parent, s.Name())
+		transaction.SpanID = sentry.SpanID(otelSpanId)
+		transaction.StartTime = s.StartTime()
+
+		ssp.SpanMap[otelSpanId] = transaction
+	}
 }
 
-func (ssp *sentrySpanProcessor) OnEnd(s trace.ReadOnlySpan) {
+func (ssp *sentrySpanProcessor) OnEnd(s sdkTrace.ReadOnlySpan) {
 }
 
 func (bsp *sentrySpanProcessor) Shutdown(ctx context.Context) error {
