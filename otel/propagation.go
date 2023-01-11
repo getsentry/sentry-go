387cccbbd00ccb7fed4d30f4bf5c18874afc35b3
Anton Ovchinnikov <anton@tonyo.info>
2023-01-11 12:56:02 +0100

    Update propagator skeleton

--- otel/propagation.go
+++ otel/propagation.go
@@ -3,23 +3,25 @@ package sentryotel
 import (
 	"context"
 
+	"github.com/getsentry/sentry-go"
 	"go.opentelemetry.io/otel/propagation"
 )
 
-type sentryPropagator []propagation.TextMapPropagator
+type sentryPropagator struct{}
 
 func (p sentryPropagator) Inject(ctx context.Context, carrier propagation.TextMapCarrier) {
+
 }
 
 func (p sentryPropagator) Extract(ctx context.Context, carrier propagation.TextMapCarrier) context.Context {
-
+	carrier.Get(sentry.SentryTraceHeader)
 	return ctx
 }
 
 func (p sentryPropagator) Fields() []string {
-	return []string{}
+	return []string{sentry.SentryTraceHeader, sentry.SentryBaggageHeader}
 }
 
-func NewSentryPropagator(p ...propagation.TextMapPropagator) propagation.TextMapPropagator {
-	return sentryPropagator(p)
+func NewSentryPropagator() propagation.TextMapPropagator {
+	return sentryPropagator{}
 }
