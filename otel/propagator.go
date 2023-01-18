6b42a3fbcdd2af8ca053cb1970c3e4b4e548e204
Anton Ovchinnikov <anton@tonyo.info>
2023-01-18 13:10:57 +0100

    Basic debug info

--- otel/propagator.go
+++ otel/propagator.go
@@ -61,6 +61,9 @@ func (p sentryPropagator) Inject(ctx context.Context, carrier propagation.TextMa
 	if finalBaggage.Len() > 0 {
 		carrier.Set(sentry.SentryBaggageHeader, finalBaggage.String())
 	}
+
+	fmt.Printf("sentry-trace header: '%s'\n", carrier.Get(sentry.SentryTraceHeader))
+	fmt.Printf("baggage header: '%s'\n", carrier.Get(sentry.SentryBaggageHeader))
 }
 
 // Extract reads cross-cutting concerns from the carrier into a Context.
@@ -68,6 +71,7 @@ func (p sentryPropagator) Extract(ctx context.Context, carrier propagation.TextM
 	fmt.Printf("\n--- Propagator Extract\nContext: %#v\nCarrier: %#v\n", ctx, carrier)
 
 	sentryTraceHeader := carrier.Get(sentry.SentryTraceHeader)
+	fmt.Printf("sentry-trace header: '%s'\n", sentryTraceHeader)
 
 	if sentryTraceHeader != "" {
 		ctx = context.WithValue(ctx, utils.SentryTraceHeaderKey(), sentryTraceHeader)
@@ -88,6 +92,7 @@ func (p sentryPropagator) Extract(ctx context.Context, carrier propagation.TextM
 	}
 
 	baggageHeader := carrier.Get(sentry.SentryBaggageHeader)
+	fmt.Printf("baggage header: '%s'\n", baggageHeader)
 	if baggageHeader != "" {
 		// Preserve the original baggage
 		parsedBaggage, err := baggage.Parse(baggageHeader)
