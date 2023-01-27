fd951027b5b515b7c8f071d492f26ca6a408fc7d
Anton Ovchinnikov <anton@tonyo.info>
2023-01-27 18:02:06 +0100

    sentryrequest: Pass context to avoid hub issues

--- otel/internal/utils/sentryrequest.go
+++ otel/internal/utils/sentryrequest.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"context"
 	"strings"
 
 	"github.com/getsentry/sentry-go"
@@ -10,28 +11,30 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 )
 
-func IsSentryRequestSpan(s trace.ReadOnlySpan) bool {
+func IsSentryRequestSpan(ctx context.Context, s trace.ReadOnlySpan) bool {
 	attributes := s.Attributes()
 
 	// TODO(michi): can we access the attribute directly?
 	for _, attribute := range attributes {
 		if attribute.Key == semconv.HTTPURLKey {
-			return isSentryRequestUrl(attribute.Value.AsString())
+			return isSentryRequestUrl(ctx, attribute.Value.AsString())
 		}
 	}
 
 	return false
 }
 
-func isSentryRequestUrl(url string) bool {
-	hub := sentry.CurrentHub()
-	client := hub.Client()
-
-	if client != nil {
-		dsn, _ := sentry.NewDsn(client.Options().Dsn)
-
-		return strings.Contains(url, dsn.GetHost())
+func isSentryRequestUrl(ctx context.Context, url string) bool {
+	hub := sentry.GetHubFromContext(ctx)
+	if hub == nil {
+		return false
 	}
 
-	return false
+	client := hub.Client()
+	if client == nil {
+		return false
+	}
+
+	dsn, _ := sentry.NewDsn(client.Options().Dsn)
+	return strings.Contains(url, dsn.GetHost())
 }
