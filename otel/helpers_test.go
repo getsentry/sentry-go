9f5114be49776f94ac04d5fc622cb082f97a653a
Anton Ovchinnikov <anton@tonyo.info>
2023-01-27 18:01:29 +0100

    helpers_test: Copy TransportMock from the root package

--- otel/helpers_test.go
+++ otel/helpers_test.go
@@ -11,7 +11,9 @@ import (
 	"log"
 	"reflect"
 	"sort"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/getsentry/sentry-go"
 	"github.com/google/go-cmp/cmp"
@@ -151,3 +153,29 @@ func otelSpanIDFromHex(s string) trace.SpanID {
 	}
 	return spanID
 }
+
+// FIXME(anton): copie from mocks_test.go
+
+type TransportMock struct {
+	mu        sync.Mutex
+	events    []*sentry.Event
+	lastEvent *sentry.Event
+}
+
+func (t *TransportMock) Configure(options sentry.ClientOptions) {}
+func (t *TransportMock) SendEvent(event *sentry.Event) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.events = append(t.events, event)
+	t.lastEvent = event
+}
+func (t *TransportMock) Flush(timeout time.Duration) bool {
+	return true
+}
+func (t *TransportMock) Events() []*sentry.Event {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return t.events
+}
+
+//
