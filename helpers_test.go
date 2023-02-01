beca7e6abda0157cf2adc6e1a106d41970c31020
Anton Ovchinnikov <anton@tonyo.info>
2023-02-01 19:26:59 +0100

    ref(test): Move assert* helpers to internal package (#562)

--- helpers_test.go
+++ helpers_test.go
@@ -1,71 +1,9 @@
 package sentry
 
 import (
-	"fmt"
-	"reflect"
-	"testing"
-
-	"github.com/getsentry/sentry-go/internal/otel/baggage"
+	"github.com/getsentry/sentry-go/internal/testutils"
 )
 
-func assertEqual(t *testing.T, got, want interface{}, userMessage ...interface{}) {
-	t.Helper()
-
-	if !reflect.DeepEqual(got, want) {
-		logFailedAssertion(t, formatUnequalValues(got, want), userMessage...)
-	}
-}
-
-func assertNotEqual(t *testing.T, got, want interface{}, userMessage ...interface{}) {
-	t.Helper()
-
-	if reflect.DeepEqual(got, want) {
-		logFailedAssertion(t, formatUnequalValues(got, want), userMessage...)
-	}
-}
-
-func logFailedAssertion(t *testing.T, summary string, userMessage ...interface{}) {
-	t.Helper()
-	text := summary
-
-	if len(userMessage) > 0 {
-		if message, ok := userMessage[0].(string); ok {
-			if message != "" && len(userMessage) > 1 {
-				text = fmt.Sprintf(message, userMessage[1:]...) + text
-			} else if message != "" {
-				text = fmt.Sprint(message) + text
-			}
-		}
-	}
-
-	t.Error(text)
-}
-
-func formatUnequalValues(got, want interface{}) string {
-	var a, b string
-
-	if reflect.TypeOf(got) != reflect.TypeOf(want) {
-		a, b = fmt.Sprintf("%T(%#v)", got, got), fmt.Sprintf("%T(%#v)", want, want)
-	} else {
-		a, b = fmt.Sprintf("%#v", got), fmt.Sprintf("%#v", want)
-	}
-
-	return fmt.Sprintf("\ngot: %s\nwant: %s", a, b)
-}
-
-func assertBaggageStringsEqual(t *testing.T, got, want string, userMessage ...interface{}) {
-	t.Helper()
-
-	baggageGot, err := baggage.Parse(got)
-	if err != nil {
-		t.Error(err)
-	}
-	baggageWant, err := baggage.Parse(want)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !reflect.DeepEqual(baggageGot, baggageWant) {
-		logFailedAssertion(t, formatUnequalValues(got, want), userMessage...)
-	}
-}
+var assertEqual = testutils.AssertEqual
+var assertNotEqual = testutils.AssertNotEqual
+var assertBaggageStringsEqual = testutils.AssertBaggageStringsEqual
--- otel/helpers_test.go
+++ otel/helpers_test.go
@@ -2,69 +2,25 @@
 
 package sentryotel
 
-// TODO(anton): This is a copy of helpers_test.go in the repo root.
-// We should figure out how to share testing helpers.
-
 import (
 	"encoding/hex"
-	"fmt"
-	"reflect"
 	"sort"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/getsentry/sentry-go"
+	"github.com/getsentry/sentry-go/internal/testutils"
 	"github.com/google/go-cmp/cmp"
 	"go.opentelemetry.io/otel/baggage"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
 )
 
-func assertEqual(t *testing.T, got, want interface{}, userMessage ...interface{}) {
-	t.Helper()
-
-	if !reflect.DeepEqual(got, want) {
-		logFailedAssertion(t, formatUnequalValues(got, want), userMessage...)
-	}
-}
-
-func assertNotEqual(t *testing.T, got, want interface{}, userMessage ...interface{}) {
-	t.Helper()
-
-	if reflect.DeepEqual(got, want) {
-		logFailedAssertion(t, formatUnequalValues(got, want), userMessage...)
-	}
-}
-
-func logFailedAssertion(t *testing.T, summary string, userMessage ...interface{}) {
-	t.Helper()
-	text := summary
-
-	if len(userMessage) > 0 {
-		if message, ok := userMessage[0].(string); ok {
-			if message != "" && len(userMessage) > 1 {
-				text = fmt.Sprintf(message, userMessage[1:]...) + text
-			} else if message != "" {
-				text = fmt.Sprint(message) + text
-			}
-		}
-	}
-
-	t.Error(text)
-}
-
-func formatUnequalValues(got, want interface{}) string {
-	var a, b string
-
-	if reflect.TypeOf(got) != reflect.TypeOf(want) {
-		a, b = fmt.Sprintf("%T(%#v)", got, got), fmt.Sprintf("%T(%#v)", want, want)
-	} else {
-		a, b = fmt.Sprintf("%#v", got), fmt.Sprintf("%#v", want)
-	}
-
-	return fmt.Sprintf("\ngot: %s\nwant: %s", a, b)
-}
+var assertEqual = testutils.AssertEqual
+var assertNotEqual = testutils.AssertNotEqual
+var assertTrue = testutils.AssertTrue
+var assertFalse = testutils.AssertFalse
 
 // assertMapCarrierEqual compares two values of type propagation.MapCarrier and raises an
 // assertion error if the values differ.
@@ -94,8 +50,6 @@ func assertMapCarrierEqual(t *testing.T, got, want propagation.MapCarrier, userM
 			if diff := cmp.Diff(wantErr, gotErr); diff != "" {
 				t.Errorf("Comparing Baggage parsing errors (-want +got):\n%s", diff)
 			}
-
-			// sortedBaggage = gotBaggage.Members()
 
 			if diff := cmp.Diff(
 				wantBaggage,
@@ -159,7 +113,13 @@ func otelSpanIDFromHex(s string) trace.SpanID {
 	return spanID
 }
 
-// FIXME(anton): copie from mocks_test.go
+// FIXME(anton): TransportMock is copied from mocks_test.go
+// I don't see an easy way right now to reuse this struct in "sentry" and
+// "sentryotel" packages: it naturally depends on "sentry", but tests in "sentry"
+// package also depend on it, so if we move it to a new package, we'll get an
+// import cycle.
+// Alternatively, it could be made public on "sentry" package, but it doesn't
+// feel right.
 
 type TransportMock struct {
 	mu        sync.Mutex
