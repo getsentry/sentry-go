fddcee656d818aaf6a162c7694ba7eea6c0774b6
Anton Ovchinnikov <anton@tonyo.info>
2023-02-15 16:16:38 +0100

    Update tests

--- otel/event_processor.go
+++ otel/event_processor.go
@@ -12,9 +12,11 @@ import (
 //
 // Caveat: hint.Context should contain a valid context populated by OpenTelemetry's span context.
 func linkTraceContextToErrorEvent(event *sentry.Event, hint *sentry.EventHint) *sentry.Event {
-	sentry.Logger.Printf("\nEvent Processor\nEvent: %#v\nHint: %#v", event, hint)
-
 	if hint == nil || hint.Context == nil {
+		return event
+	}
+	// TODO: what we want here is to compare with the (unexported) transactionType
+	if event.Type == "transaction" {
 		return event
 	}
 	otelSpanContext := trace.SpanContextFromContext(hint.Context)
--- otel/event_processor_test.go
+++ otel/event_processor_test.go
@@ -4,65 +4,54 @@ package sentryotel
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/getsentry/sentry-go"
 )
 
-func TestLinkTraceContextToErrorEventWithEmptyTraceContext(t *testing.T) {
-	_, _, tracer := setupSpanProcessorTest()
-	ctx, otelSpan := tracer.Start(emptyContextWithSentry(), "spanName")
-	sentrySpan, _ := sentrySpanMap.Get(otelSpan.SpanContext().SpanID())
+func TestLinkTraceContextToErrorEventSetsContext(t *testing.T) {
 
-	hub := sentry.GetHubFromContext(ctx)
-	client := hub.Client()
-	client.CaptureException(
-		errors.New("new sentry exception"),
-		&sentry.EventHint{Context: ctx},
-		nil,
-	)
+	withExistingContextOptions := []bool{false, true}
 
-	transport := client.Transport.(*TransportMock)
-	events := transport.Events()
-	assertEqual(t, len(events), 1)
-	err := events[0]
-	exception := err.Exception[0]
-	assertEqual(t, exception.Type, "*errors.errorString")
-	assertEqual(t, exception.Value, "new sentry exception")
-	assertEqual(t, err.Type, "")
-	assertEqual(t,
-		err.Contexts["trace"],
-		map[string]interface{}{
-			"trace_id":       sentrySpan.TraceID.String(),
-			"span_id":        sentrySpan.SpanID.String(),
-			"parent_span_id": sentrySpan.ParentSpanID.String(),
-		},
-	)
-}
+	for _, withExistingContext := range withExistingContextOptions {
+		withExistingContext := withExistingContext
+		name := fmt.Sprintf("withExistingContext_%v", withExistingContext)
 
-func TestLinkTraceContextToErrorEventDoesNotTouchExistingTraceContext(t *testing.T) {
-	_, _, tracer := setupSpanProcessorTest()
-	ctx, _ := tracer.Start(emptyContextWithSentry(), "spanName")
+		t.Run(name, func(t *testing.T) {
+			_, _, tracer := setupSpanProcessorTest()
+			ctx, otelSpan := tracer.Start(emptyContextWithSentry(), "spanName")
+			sentrySpan, _ := sentrySpanMap.Get(otelSpan.SpanContext().SpanID())
 
-	hub := sentry.GetHubFromContext(ctx)
-	hub.Scope().SetContext("trace", map[string]interface{}{"trace_id": "123"})
-	client := hub.Client()
-	client.CaptureException(
-		errors.New("new sentry exception with existing trace context"),
-		&sentry.EventHint{Context: ctx},
-		hub.Scope(),
-	)
+			hub := sentry.GetHubFromContext(ctx)
+			client, scope := hub.Client(), hub.Scope()
 
-	transport := client.Transport.(*TransportMock)
-	events := transport.Events()
-	assertEqual(t, len(events), 1)
-	err := events[0]
-	exception := err.Exception[0]
-	assertEqual(t, exception.Type, "*errors.errorString")
-	assertEqual(t, exception.Value, "new sentry exception with existing trace context")
-	assertEqual(t, err.Type, "")
-	assertEqual(t,
-		err.Contexts["trace"],
-		map[string]interface{}{"trace_id": "123"},
-	)
+			if withExistingContext {
+				// The existing "trace" context should be ovewritten by the event processor
+				scope.SetContext("trace", map[string]interface{}{"trace_id": "123"})
+			}
+			client.CaptureException(
+				errors.New("new sentry exception with existing trace context"),
+				&sentry.EventHint{Context: ctx},
+				hub.Scope(),
+			)
+
+			transport := client.Transport.(*TransportMock)
+			events := transport.Events()
+			assertEqual(t, len(events), 1)
+			err := events[0]
+			exception := err.Exception[0]
+			assertEqual(t, exception.Type, "*errors.errorString")
+			assertEqual(t, exception.Value, "new sentry exception with existing trace context")
+			assertEqual(t, err.Type, "")
+			assertEqual(t,
+				err.Contexts["trace"],
+				map[string]interface{}{
+					"trace_id":       sentrySpan.TraceID.String(),
+					"span_id":        sentrySpan.SpanID.String(),
+					"parent_span_id": sentrySpan.ParentSpanID.String(),
+				},
+			)
+		})
+	}
 }
