8b47c36351ea67738b5909fd4af9de910d6614ba
Joshua Wise <joshua@accelerated.tech>
2023-03-28 15:22:11 -0700

    test: add individual tests for new stacktrace and debugmeta interfaces

--- stacktrace_test.go
+++ stacktrace_test.go
@@ -1,6 +1,7 @@
 package sentry
 
 import (
+	"encoding/json"
 	"errors"
 	"testing"
 
@@ -167,5 +168,77 @@ func TestExtractXErrorsPC(t *testing.T) {
 	// appropriate type of errors, see stacktrace_external_test.go.
 	if got := extractXErrorsPC(errors.New("test")); got != nil {
 		t.Errorf("got %#v, want nil", got)
+	}
+}
+
+func TestEventWithExceptionStacktraceMarshalJSON(t *testing.T) {
+	event := NewEvent()
+	event.Exception = []Exception{
+		{
+			Stacktrace: &Stacktrace{
+				Frames: []Frame{
+					{
+						Function:    "gofunc",
+						Symbol:      "gosym",
+						Module:      "gopkg/gopath",
+						Filename:    "foo.go",
+						AbsPath:     "/something/foo.go",
+						Lineno:      35,
+						Colno:       72,
+						PreContext:  []string{"pre", "context"},
+						ContextLine: "contextline",
+						PostContext: []string{"post", "context"},
+						InApp:       true,
+						Vars: map[string]interface{}{
+							"foostr": "bar",
+							"fooint": 25,
+						},
+					},
+					{
+						Symbol:          "nativesym",
+						Package:         "my.dylib",
+						InstructionAddr: "0xabcd0010",
+						AddrMode:        "abs",
+						SymbolAddr:      "0xabcd0000",
+						ImageAddr:       "0xabc00000",
+						Platform:        "native",
+						StackStart:      false,
+					},
+				},
+			},
+		},
+	}
+
+	got, err := json.Marshal(event)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := `{"sdk":{},"user":{},` +
+		`"exception":[{"stacktrace":{"frames":[` +
+		`{"function":"gofunc",` +
+		`"symbol":"gosym",` +
+		`"module":"gopkg/gopath",` +
+		`"filename":"foo.go",` +
+		`"abs_path":"/something/foo.go",` +
+		`"lineno":35,` +
+		`"colno":72,` +
+		`"pre_context":["pre","context"],` +
+		`"context_line":"contextline",` +
+		`"post_context":["post","context"],` +
+		`"in_app":true,` +
+		`"vars":{"fooint":25,"foostr":"bar"}` +
+		`},{` +
+		`"symbol":"nativesym",` +
+		`"package":"my.dylib",` +
+		`"instruction_addr":"0xabcd0010",` +
+		`"addr_mode":"abs",` +
+		`"symbol_addr":"0xabcd0000",` +
+		`"image_addr":"0xabc00000",` +
+		`"platform":"native"` +
+		`}]}}]}`
+
+	if diff := cmp.Diff(want, string(got)); diff != "" {
+		t.Errorf("Event mismatch (-want +got):\n%s", diff)
 	}
 }
