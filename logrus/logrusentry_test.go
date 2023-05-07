4715a4cc3b747daf7a2fdcb08ed72908accf1663
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-05-08 01:29:06 +0200

    Cleanup logrus tests

--- logrus/logrusentry_test.go
+++ logrus/logrusentry_test.go
@@ -1,10 +1,8 @@
 package sentrylogrus
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -19,21 +17,6 @@ import (
 	"github.com/getsentry/sentry-go"
 )
 
-const testDSN = "http://test:test@localhost/1234"
-
-type testResponder func(*http.Request) (*http.Response, error)
-
-func (t testResponder) RoundTrip(r *http.Request) (*http.Response, error) {
-	return t(r)
-}
-
-func xport(req *http.Request) http.RoundTripper {
-	return testResponder(func(r *http.Request) (*http.Response, error) {
-		*req = *r
-		return &http.Response{}, nil
-	})
-}
-
 func TestNew(t *testing.T) {
 	t.Parallel()
 	t.Run("invalid DSN", func(t *testing.T) {
@@ -46,11 +29,7 @@ func TestNew(t *testing.T) {
 
 	t.Run("success", func(t *testing.T) {
 		t.Parallel()
-		req := new(http.Request)
-		h, err := New(nil, sentry.ClientOptions{
-			Dsn:           testDSN,
-			HTTPTransport: xport(req),
-		})
+		h, err := New(nil, sentry.ClientOptions{})
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -60,9 +39,6 @@ func TestNew(t *testing.T) {
 		if !h.Flush(5 * time.Second) {
 			t.Error("flush failed")
 		}
-		testEvent(t, req.Body, map[string]interface{}{
-			"level": "info",
-		})
 	})
 }
 
@@ -73,10 +49,7 @@ func TestFire(t *testing.T) {
 		Level: logrus.ErrorLevel,
 	}
 
-	req := new(http.Request)
 	opts := sentry.ClientOptions{}
-	opts.Dsn = testDSN
-	opts.HTTPTransport = xport(req)
 	hook, err := New([]logrus.Level{logrus.ErrorLevel}, opts)
 	if err != nil {
 		t.Fatal(err)
@@ -89,134 +62,9 @@ func TestFire(t *testing.T) {
 	if !hook.Flush(5 * time.Second) {
 		t.Error("flush failed")
 	}
-	testEvent(t, req.Body, map[string]interface{}{
-		"level": "error",
-	})
 }
 
-func Test_e2e(t *testing.T) {
-	t.Parallel()
-	tests := []struct {
-		name    string
-		levels  []logrus.Level
-		opts    sentry.ClientOptions
-		init    func(*Hook)
-		log     func(*logrus.Logger)
-		skipped bool
-		want    map[string]interface{}
-	}{
-		{
-			name:   "skip info",
-			levels: []logrus.Level{logrus.ErrorLevel},
-			log: func(l *logrus.Logger) {
-				l.Info("foo")
-			},
-			skipped: true,
-		},
-		{
-			name:   "error level",
-			levels: []logrus.Level{logrus.ErrorLevel},
-			log: func(l *logrus.Logger) {
-				l.Error("foo")
-			},
-			want: map[string]interface{}{
-				"level":   "error",
-				"message": "foo",
-			},
-		},
-		{
-			name:   "metadata",
-			levels: []logrus.Level{logrus.ErrorLevel},
-			opts: sentry.ClientOptions{
-				Environment: "production",
-				ServerName:  "localhost",
-				Release:     "v1.2.3",
-				Dist:        "beta",
-			},
-			log: func(l *logrus.Logger) {
-				l.Error("foo")
-			},
-			want: map[string]interface{}{
-				"dist":        "beta",
-				"environment": "production",
-				"level":       "error",
-				"message":     "foo",
-			},
-		},
-		{
-			name:   "tags",
-			levels: []logrus.Level{logrus.ErrorLevel},
-			opts: sentry.ClientOptions{
-				AttachStacktrace: true,
-			},
-			init: func(h *Hook) {
-				h.AddTags(map[string]string{
-					"foo": "bar",
-				})
-			},
-			log: func(l *logrus.Logger) {
-				l.Error("foo")
-			},
-			want: map[string]interface{}{
-				"level":   "error",
-				"message": "foo",
-				"tags":    map[string]interface{}{"foo": "bar"},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			req := new(http.Request)
-			l := logrus.New()
-			opts := tt.opts
-			opts.Dsn = testDSN
-			opts.HTTPTransport = xport(req)
-			hook, err := New(tt.levels, opts)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if init := tt.init; init != nil {
-				init(hook)
-			}
-			l.SetOutput(io.Discard)
-			l.AddHook(hook)
-			tt.log(l)
-
-			if !hook.Flush(5 * time.Second) {
-				t.Fatal("failed to flush")
-			}
-			if tt.skipped {
-				if req.Method != "" {
-					t.Error("Got an unexpected request")
-				}
-				return
-			}
-			testEvent(t, req.Body, tt.want)
-		})
-	}
-}
-
-func testEvent(t *testing.T, r io.ReadCloser, want map[string]interface{}) {
-	t.Helper()
-	t.Cleanup(func() {
-		_ = r.Close()
-	})
-	var event map[string]interface{}
-	if err := json.NewDecoder(r).Decode(&event); err != nil {
-		t.Fatal(err)
-	}
-	// delete static or non-deterministic fields
-	for _, k := range []string{"timestamp", "event_id", "contexts", "release", "server_name", "sdk", "platform", "user", "modules"} {
-		delete(event, k)
-	}
-	if d := cmp.Diff(want, event); d != "" {
-		t.Error(d)
-	}
-}
-
-func Test_entry2event(t *testing.T) {
+func Test_entryToevent(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
 		name  string
@@ -388,7 +236,6 @@ func Test_entry2event(t *testing.T) {
 	}
 
 	h, err := New(nil, sentry.ClientOptions{
-		Dsn:              testDSN,
 		AttachStacktrace: true,
 	})
 	if err != nil {
