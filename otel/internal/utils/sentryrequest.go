ce90464c17afeb628a4047ba05ada1729bfc760f
Michi Hoffmann <cleptric@users.noreply.github.com>
2023-05-08 11:10:29 +0200

    ref: Improve isSentryRequestUrl (#636)

--- otel/internal/utils/sentryrequest.go
+++ otel/internal/utils/sentryrequest.go
@@ -38,6 +38,10 @@ func isSentryRequestUrl(ctx context.Context, url string) bool {
 		return false
 	}
 
-	dsn, _ := sentry.NewDsn(client.Options().Dsn)
-	return strings.Contains(url, dsn.GetHost()) && strings.Contains(url, dsn.GetProjectID())
+	dsn, err := sentry.NewDsn(client.Options().Dsn)
+	if err != nil {
+		return false
+	}
+
+	return strings.Contains(url, dsn.GetAPIURL().String())
 }
