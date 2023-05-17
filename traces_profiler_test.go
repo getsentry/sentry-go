8417a7bdf3462217a959ea9e1f4e2e0876dc235f
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-05-17 20:19:10 +0200

    update tests

--- traces_profiler_test.go
+++ traces_profiler_test.go
@@ -7,25 +7,36 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTraceProfiling(t *testing.T) {
+func testTraceProfiling(t *testing.T, rate float64) (*Span, *Event) {
 	transport := &TransportMock{}
 	ctx := NewTestContext(ClientOptions{
 		Transport:          transport,
 		EnableTracing:      true,
 		TracesSampleRate:   1.0,
-		ProfilesSampleRate: 1.0,
+		ProfilesSampleRate: rate,
 	})
 	span := StartSpan(ctx, "top")
 	doWorkFor(100 * time.Millisecond)
 	span.Finish()
 
+	require.Equal(t, 1, len(transport.events))
+	return span, transport.events[0]
+}
+
+func TestTraceProfiling(t *testing.T) {
 	var require = require.New(t)
-	require.Equal(1, len(transport.events))
-	var event = transport.events[0]
+	span, event := testTraceProfiling(t, 1.0)
 	require.Equal(transactionType, event.Type)
 	require.NotNil(event.transactionProfile)
 	var profileInfo = event.transactionProfile
 	require.Equal("go", profileInfo.Platform)
 	require.Equal(span.TraceID.String(), profileInfo.Transaction.TraceID)
 	validateProfile(t, profileInfo.Trace, span.EndTime.Sub(span.StartTime))
+}
+
+func TestTraceProfilingDisabled(t *testing.T) {
+	var require = require.New(t)
+	_, event := testTraceProfiling(t, 0)
+	require.Equal(transactionType, event.Type)
+	require.Nil(event.transactionProfile)
 }
