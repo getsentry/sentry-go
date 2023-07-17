73d3bdd117c6bff947a9baa04d7bdc0f7e96e765
Reinaldy Rafli <aldy505@proton.me>
2023-07-17 19:31:27 +0700

    test: check in on hub

--- hub_test.go
+++ hub_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -190,6 +191,18 @@ func TestLastEventIDUpdatesAfterCaptures(t *testing.T) {
 
 	eventID := hub.CaptureEvent(&Event{Message: "wat"})
 	assertEqual(t, *eventID, hub.LastEventID())
+
+	checkInID := hub.CaptureCheckIn(&CheckIn{
+		MonitorSlug: "job",
+		Status:      CheckInStatusOK,
+		Duration:    time.Second * 10,
+	}, &MonitorConfig{
+		Schedule:      CrontabSchedule("8 * * * *"),
+		CheckInMargin: 100,
+		MaxRuntime:    200,
+		Timezone:      "Asia/Singapore",
+	})
+	assertEqual(t, *checkInID, hub.LastEventID())
 }
 
 func TestLastEventIDNotChangedForTransactions(t *testing.T) {
