826105fe2c72f393d8e1e8d3b6fbb813ccc1a2c7
Emir Ribić <e@ribic.ba>
2024-07-30 17:22:31 +0200

    linter fixes

--- slog/common_test.go
+++ slog/common_test.go
@@ -374,6 +374,8 @@ func TestValueToString(t *testing.T) {
 	}
 }
 
+type ctxKey string
+
 func TestContextExtractor(t *testing.T) {
 	tests := map[string]struct {
 		ctx      context.Context
@@ -407,10 +409,10 @@ func TestContextExtractor(t *testing.T) {
 			expected: []slog.Attr{slog.String("key1", "value1"), slog.String("key2", "value2")},
 		},
 		"FunctionWithContext": {
-			ctx: context.WithValue(context.Background(), "userID", "1234"),
+			ctx: context.WithValue(context.Background(), ctxKey("userID"), "1234"),
 			fns: []func(ctx context.Context) []slog.Attr{
 				func(ctx context.Context) []slog.Attr {
-					if userID, ok := ctx.Value("userID").(string); ok {
+					if userID, ok := ctx.Value(ctxKey("userID")).(string); ok {
 						return []slog.Attr{slog.String("userID", userID)}
 					}
 					return []slog.Attr{}
