eb74cc130496b3cae57c93eeab44e8aa197cba31
Emir Ribić <e@ribic.ba>
2024-11-08 19:20:34 +0100

    Update min go version to 1.21 (#898)
    * bump go version to 1.21
    
    * update changelog
    
    * tidy otel go.mod

--- stacktrace_test.go
+++ stacktrace_test.go
@@ -228,3 +228,100 @@ func TestEventWithExceptionStacktraceMarshalJSON(t *testing.T) {
 		t.Errorf("Event mismatch (-want +got):\n%s", diff)
 	}
 }
+
+func TestFilterCompilerGeneratedSymbols(t *testing.T) {
+	tests := []struct {
+		symbol              string
+		expectedPackageName string
+	}{
+		{"type:.eq.[9]debug/elf.intName", ""},
+		{"type:.hash.debug/elf.ProgHeader", ""},
+		{"type:.eq.runtime._panic", ""},
+		{"type:.hash.struct { runtime.gList; runtime.n int32 }", ""},
+		{"go:(*struct { sync.Mutex; math/big.table [64]math/big", ""},
+		{"go.uber.org/zap/buffer.(*Buffer).AppendString", "go.uber.org/zap/buffer"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.symbol, func(t *testing.T) {
+			packageName := packageName(tt.symbol)
+			if diff := cmp.Diff(tt.expectedPackageName, packageName); diff != "" {
+				t.Errorf("Package name mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_cleanupFunctionNamePrefix(t *testing.T) {
+	cases := map[string]struct {
+		f    []Frame
+		want []Frame
+	}{
+		"SimpleCase": {
+			f: []Frame{
+				{Function: "main.main"},
+				{Function: "main.main.func1"},
+			},
+			want: []Frame{
+				{Function: "main.main"},
+				{Function: "func1"},
+			},
+		},
+		"MultipleLevels": {
+			f: []Frame{
+				{Function: "main.main"},
+				{Function: "main.main.func1"},
+				{Function: "main.main.func1.func2"},
+			},
+			want: []Frame{
+				{Function: "main.main"},
+				{Function: "func1"},
+				{Function: "func2"},
+			},
+		},
+		"PrefixWithRun": {
+			f: []Frame{
+				{Function: "Run.main"},
+				{Function: "Run.main.func1"},
+			},
+			want: []Frame{
+				{Function: "Run.main"},
+				{Function: "func1"},
+			},
+		},
+		"NoPrefixMatch": {
+			f: []Frame{
+				{Function: "main.main"},
+				{Function: "main.handler"},
+			},
+			want: []Frame{
+				{Function: "main.main"},
+				{Function: "main.handler"},
+			},
+		},
+		"SingleFrame": {
+			f: []Frame{
+				{Function: "main.main"},
+			},
+			want: []Frame{
+				{Function: "main.main"},
+			},
+		},
+		"ComplexPrefix": {
+			f: []Frame{
+				{Function: "app.package.Run"},
+				{Function: "app.package.Run.Logger.func1"},
+			},
+			want: []Frame{
+				{Function: "app.package.Run"},
+				{Function: "Logger.func1"},
+			},
+		},
+	}
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := cleanupFunctionNamePrefix(tt.f)
+			assertEqual(t, got, tt.want)
+		})
+	}
+}
