6021054709144f748927352e918da4e8d5474ebe
Giannis Gkiortzis <58184179+giortzisg@users.noreply.github.com>
2026-02-06 13:24:58 +0100

    enable client reports by default

--- client.go
+++ client.go
@@ -249,8 +249,8 @@ type ClientOptions struct {
 	EnableLogs bool
 	// DisableMetrics controls when metrics should be emitted.
 	DisableMetrics bool
-	// SendClientReports controls when client reports should be emitted.
-	SendClientReports bool
+	// DisableClientReports controls when client reports should be emitted.
+	DisableClientReports bool
 	// TraceIgnoreStatusCodes is a list of HTTP status codes that should not be traced.
 	// Each element can be either:
 	// - A single-element slice [code] for a specific status code
@@ -408,7 +408,7 @@ func NewClient(options ClientOptions) (*Client, error) {
 		client.batchMeter.Start()
 	}
 
-	clientreport.SetEnabled(options.SendClientReports)
+	clientreport.SetEnabled(!options.DisableClientReports)
 	client.setupIntegrations()
 
 	return &client, nil
