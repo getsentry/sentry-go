f33a7bc6b967824fda94235a591d160664a2a35e
Giannis Gkiortzis <58184179+giortzisg@users.noreply.github.com>
2026-02-12 13:38:03 +0100

    remove envelope dead code

--- internal/protocol/envelope.go
+++ internal/protocol/envelope.go
@@ -21,7 +21,7 @@ type EnvelopeHeader struct {
 
 	// SentAt is the timestamp when the event was sent from the SDK as string in RFC 3339 format.
 	// Used for clock drift correction of the event timestamp. The time zone must be UTC.
-	SentAt time.Time `json:"sent_at,omitempty"`
+	SentAt time.Time `json:"sent_at,omitzero"`
 
 	// Dsn can be used for self-authenticated envelopes.
 	// This means that the envelope has all the information necessary to be sent to sentry.
@@ -167,12 +167,6 @@ func (e *Envelope) Size() (int, error) {
 		return 0, err
 	}
 	return len(data), nil
-}
-
-// MarshalJSON converts the EnvelopeHeader to JSON.
-func (h *EnvelopeHeader) MarshalJSON() ([]byte, error) {
-	type header EnvelopeHeader
-	return json.Marshal((*header)(h))
 }
 
 // NewEnvelopeItem creates a new envelope item with the specified type and payload.
