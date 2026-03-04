6a4abf08603df9fdf774dbf35fbf4a580cb42ba2
Giannis Gkiortzis <58184179+giortzisg@users.noreply.github.com>
2026-03-04 12:27:55 +0100

    add invalid org id check

--- client_test.go
+++ client_test.go
@@ -910,6 +910,17 @@ func TestClient_ParseOrgID(t *testing.T) {
 	assert.Equal(t, uint64(1), c.dsn.GetOrgID(), "Custom org id should override the DSN parsed one")
 }
 
+func TestClient_ParseOrgIDInvalid(t *testing.T) {
+	c, err := NewClient(ClientOptions{
+		// org id is MaxUint64 + 1, should be considered empty
+		Dsn: "https://example@o18446744073709551616.ingest.us.sentry.io/1337",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, uint64(0), c.dsn.GetOrgID(), "Custom org id should override the DSN parsed one")
+}
+
 func TestClientOptions_OrgIDShouldOverrideParsed(t *testing.T) {
 	c, err := NewClient(ClientOptions{
 		Dsn:   "https://example@o1.ingest.us.sentry.io/1337",
