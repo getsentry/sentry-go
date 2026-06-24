9f94e99320696f2adae41f536e9a1dcb7ed72e56
Giannis Gkiortzis <58184179+giortzisg@users.noreply.github.com>
2026-06-24 09:26:21 +0200

    remove unnecessary alloc

--- interfaces.go
+++ interfaces.go
@@ -254,9 +254,16 @@ func newRequest(r *http.Request, client *Client) *Request {
 	}
 	url := fmt.Sprintf("%s://%s%s", prot, r.Host, r.URL.Path)
 
-	dc := snapshotDataCollection(nil, false)
+	legacyPII := false
+	if client != nil {
+		legacyPII = client.options.SendDefaultPII // nolint: staticcheck // intended use for backwards compatibility
+	}
+
+	var dc DataCollection
 	if client != nil && client.options.DataCollection != nil {
 		dc = *client.options.DataCollection
+	} else {
+		dc = snapshotDataCollection(nil, legacyPII)
 	}
 	cookies := dc.FilterCookies(r.Header.Get("Cookie"))
 	headers := make(map[string]string, len(r.Header)+1)
